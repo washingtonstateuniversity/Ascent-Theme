c6c7877d2f8b48f75f07292adde659295eff8519
jeremyfelt <jeremy.felt@gmail.com>
2015-01-29 09:51:01 -0800

    Additional prefixing to exist in an ascent namespace globally

--- functions.php
+++ functions.php
@@ -3,13 +3,13 @@
 // Add extensions of the University Center project model.
 include_once( __DIR__ . '/includes/ascent-uc-project.php' );
 
-add_action( 'wp_enqueue_scripts', 'sandbox_child_enqueue_scripts'); 
+add_action( 'wp_enqueue_scripts', 'ascent_sandbox_child_enqueue_scripts');
 /**
  * Enqueue custom scripting in child theme.
  */
-function sandbox_child_enqueue_scripts() {
-	wp_enqueue_style( 'asc', get_stylesheet_directory_uri() . '/scss/asc.css' );
-	wp_enqueue_script( 'cu', get_stylesheet_directory_uri() . '/js/cu.js', array( 'jquery' ), spine_get_script_version(), true );
+function ascent_sandbox_child_enqueue_scripts() {
+	wp_enqueue_style( 'ascent-style', get_stylesheet_directory_uri() . '/scss/asc.css' );
+	wp_enqueue_script( 'ascent-cu', get_stylesheet_directory_uri() . '/js/cu.js', array( 'jquery' ), spine_get_script_version(), true );
 }
 /* 
 * Add HTML5 search box on the side bar menu
@@ -18,7 +18,7 @@ add_theme_support( 'html5', array( 'search-form' ) );
 /*
 * Override with new custom favicon
 */
-function blog_favicon() {
+function ascent_blog_favicon() {
 	echo '<link rel="Shortcut Icon" type="image/x-icon" href="'.get_bloginfo('wpurl').'wp-content/themes/ascent/favicon.ico" />';
 }
-add_action('wp_head', 'blog_favicon');
+add_action('wp_head', 'ascent_blog_favicon');

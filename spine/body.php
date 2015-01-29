0d312b993e4e8327d903f6f6be01f93d7399776a
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2015-01-29 15:21:20 -0800

    Add feature side post
    * Add excerpt with title on sidebar.
    * Adjust post page desktop view
    * Adjust post page mobile view

--- spine/body.php
+++ spine/body.php
@@ -22,26 +22,27 @@ if ( true == spine_get_option( 'crop' ) && is_front_page() ) {
 		<h4>Latest news item</h4>
 		<div class="side-latest-news-box">
 		<?php
-			$args = array(
+				$args = array(
 				'posts_per_page' => 1,
-				'offset'=> 0,
 				'post_type' => 'post',
 				'tax_query' => array(
 					array(
 						'taxonomy' => 'category',
 						'field' => 'slug',
-						'terms' => 'test'
+						'terms' => 'side-bar'
 					),
 				),
 			);
 			$my_posts = new WP_Query( $args );
 			if ( $my_posts->have_posts() ) : while( $my_posts->have_posts() ) : $my_posts->the_post();
 				?>
-				<span class="blog-excerpt"><?php the_excerpt(); ?></span>
+				<h5 class="blog-side-title"><?php echo get_the_title(); ?></h5>
+				<span class="blog-excerpt"><?php echo wp_trim_words( get_the_excerpt(), 16, '...' ); ?></span>
 				<span class="rmore"><a href="<?php the_permalink(); ?>">More</a></span>
 			<?php endwhile; endif;
 			wp_reset_query();
 			?>
+
 </div>
 
 	</section>

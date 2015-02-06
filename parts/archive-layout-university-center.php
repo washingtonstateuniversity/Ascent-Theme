dbcbf4477709bb3a3a079dae5ea276edec8a5e58
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2015-02-06 14:34:18 -0800

    Remove the_title
    the_title was just listing the first item on the list and not the
    actual title of the page

--- parts/archive-layout-university-center.php
+++ parts/archive-layout-university-center.php
@@ -1,8 +1,6 @@
 <section class="row single gutter pad-ends">
 	<div class="column one">
-		<header>
-				<h1 class="article-title"><?php the_title(); ?></h1>
-		</header>
+	
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'articles/post', get_post_type() ); ?>
 

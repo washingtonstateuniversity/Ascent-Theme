d0eeafe97b91c3827b75b61e8f876153c2417f94
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2015-02-09 14:45:32 -0800

    Add titles to UC pages, css changes
    *Add titles to University Partners, Advisory Committee, Projects, and
    Topics
    * Adjust SCSS for headings

--- parts/archive-layout-university-center.php
+++ parts/archive-layout-university-center.php
@@ -1,12 +1,24 @@
 <section class="row single gutter pad-ends">
 	<div class="column one">
 <?php
-if ( is_post_type_archive('wsuwp_uc_entity') ) {
-    ?>
-    <header>
-    	<h1>University Partners</h1>
-	</header>
-    <?php
+if (is_post_type_archive( 'wsuwp_uc_entity' )) {
+  echo "<header>
+  <h1>University Partners</h1>
+  </header>'";
+} else if ( is_post_type_archive( 'wsuwp_uc_person' )) {
+ echo "<header>
+  <h1>Advisory Committee</h1>
+  </header>";
+}
+else if ( is_post_type_archive( 'wsuwp_uc_project' )) {
+ echo "<header>
+  <h1>Projects</h1>
+  </header>";
+}
+else if ( is_post_type_archive( 'wsuwp_uc_topic' )) {
+ echo "<header>
+  <h1>Topics</h1>
+  </header>";
 }
 ?>
 		<?php while ( have_posts() ) : the_post(); ?>

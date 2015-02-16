34f562a2b84361cfdcd5ffb5f56e5d5d713894ae
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2015-02-16 11:58:23 -0800

    Adds titles to Topics list
    Adds titles to Topics pages
    Adjust styles to condense the SASS

--- parts/archive-layout-university-center.php
+++ parts/archive-layout-university-center.php
@@ -15,11 +15,6 @@ else if ( is_post_type_archive( 'wsuwp_uc_project' )) {
   <h1>Projects</h1>
   </header>";
 }
-else if ( is_post_type_archive( 'wsuwp_uc_topic' )) {
- echo "<header>
-  <h1>Topics</h1>
-  </header>";
-}
 ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'articles/post', get_post_type() ); ?>
---
+++ taxonomy-wsuwp_uc_topics.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+
+<main class="spine-page-default">
+
+<?php get_template_part('parts/headers'); ?>
+<?php get_template_part('parts/featured-images'); ?>
+
+<section class="row side-right gutter pad-ends">
+
+	<div class="column one">
+<?php
+if (is_tax( 'wsuwp_uc_topics', 'alternative-fuels' )) {
+  echo "<header>
+  <h1>Alternative Fuels</h1>
+  </header>";
+} else if (is_tax( 'wsuwp_uc_topics', 'noise' )) {
+ echo "<header>
+  <h1>Noise</h1>
+  </header>";
+}
+else if (is_tax( 'wsuwp_uc_topics', 'emissionsclimate' )) {
+ echo "<header>
+  <h1>Noise</h1>
+  </header>";
+}
+else if (is_tax( 'wsuwp_uc_topics', 'design-and-operations' )) {
+ echo "<header>
+  <h1>Design and Operations</h1>
+  </header>";
+}
+?>
+	<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'articles/post', get_post_type() ); ?>
+
+		<?php endwhile; // end of the loop. ?>
+
+	</div><!--/column-->
+
+</section>
+
+</main>
+
+<?php get_footer(); ?>

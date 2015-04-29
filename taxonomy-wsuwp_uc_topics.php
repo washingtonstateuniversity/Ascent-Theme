55f8cecc55189e904a37f0c5463ac04251a4cbc9
jeremyfelt <jeremy.felt@gmail.com>
2015-04-29 15:02:06 -0700

    Remove reliance on specific term slugs for topic title display

--- taxonomy-wsuwp_uc_topics.php
+++ taxonomy-wsuwp_uc_topics.php
@@ -1,47 +1,22 @@
 <?php get_header(); ?>
 
 <main class="spine-page-default">
+	<?php get_template_part('parts/headers'); ?>
+	<?php get_template_part('parts/featured-images'); ?>
 
-<?php get_template_part('parts/headers'); ?>
-<?php get_template_part('parts/featured-images'); ?>
-	<section class="row single gutter pad-ends">
+	<section class="row side-right gutter pad-ends">
 		<div class="column one">
+			<header>
+				<h1><?php echo esc_html( single_term_title( '', false ) ); ?></h1>
+			</header>
 			<p><?php echo wp_kses_post( term_description() ); ?></p>
-		</div>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'articles/post', get_post_type() ); ?>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!--/column-->
 	</section>
-<section class="row side-right gutter pad-ends">
-
-	<div class="column one">
-<?php
-if (is_tax( 'wsuwp_uc_topics', 'alternative-fuels' )) {
-  echo "<header>
-  <h1>Alternative Fuels</h1>
-  </header>";
-} else if (is_tax( 'wsuwp_uc_topics', 'noise' )) {
- echo "<header>
-  <h1>Noise</h1>
-  </header>";
-}
-else if (is_tax( 'wsuwp_uc_topics', 'emissionsclimate' )) {
- echo "<header>
-  <h1>Emissions/Climate</h1>
-  </header>";
-}
-else if (is_tax( 'wsuwp_uc_topics', 'design-and-operations' )) {
- echo "<header>
-  <h1>Design and Operations</h1>
-  </header>";
-}
-?>
-	<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'articles/post', get_post_type() ); ?>
-
-		<?php endwhile; // end of the loop. ?>
-
-	</div><!--/column-->
-
-</section>
-
 </main>
 
-<?php get_footer(); ?>
+<?php get_footer();

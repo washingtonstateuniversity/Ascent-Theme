1c830c54215e9b41bbf004643806b924d3e59c34
jeremyfelt <jeremy.felt@gmail.com>
2015-04-29 11:51:13 -0700

    Extend projects to include associated people as project leads
    Uses the same method used for assigning FAA Advisors

--- js/admin.js
+++ js/admin.js
@@ -4,5 +4,9 @@
 			window.wsuwp_uc_autocomplete_object( 'faa_advisors' );
 			$('#faa_advisors-results').on('click', '.uc-object-close','faa_advisors',window.wsuwp_uc_remove_object);
 		}
+		if ( 0 !== $('#ascent-assign-project-leads').length) {
+			window.wsuwp_uc_autocomplete_object( 'project_leads' );
+			$('#project_leads-results').on('click', '.uc-object-close','project_leads',window.wsuwp_uc_remove_object);
+		}
 	});
 }(jQuery,window));

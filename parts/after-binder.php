32179535123c5208a1196fa5822976195dec37d8
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2015-08-26 10:21:56 -0700

    Main footer
    Separates the footer into three lines
    Adds spacing above and below the ul
    Prevents menu secondary from hovering on parent

--- parts/after-binder.php
+++ parts/after-binder.php
@@ -1,9 +1,9 @@
 <footer class="main-foot">
-	<ul>
-		<li>FAA Center of Excellence for Alternative Jet Fuels &amp; Environment</li>
+		<span>FAA Center of Excellence for Alternative Jet Fuels &amp; Environment</span>
+		<ul>
 		<li><a href="http://wsu.edu">Washington State University</a></li>
 		<li><a href="http://mit.edu">Massachusetts Institute of Technology</a></li>
 		<li><a href="http://www.faa.gov">Federal Aviation Administration</a></li>
-		<li>© Washington State University</li>
 	</ul>
+			<span>© Washington State University</span>
 </footer>

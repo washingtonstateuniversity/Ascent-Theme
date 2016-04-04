ead137346fe441de25afda1d46c730ab1ac4aa69
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2016-04-04 13:30:26 -0700

    0.9.24 ASCENT theme
    * Adds grunt file
    * Adjusts footer to remove -margin
    * Removes excessive padding on main

---
+++ gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'style/asc.css' : 'sass/asc.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '.css/.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+};
